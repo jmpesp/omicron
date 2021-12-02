4e1a83d9095561713722dde2373a74c9ba57fd4b
Andrew J. Stone <andrew@oxidecomputer.com>
2021-12-02 12:14:44 -0500

    Move protocol doc to trust_quorum mod.rs

--- sled-agent/src/bootstrap/trust_quorum/mod.rs
+++ sled-agent/src/bootstrap/trust_quorum/mod.rs
@@ -2,6 +2,31 @@
 //!
 //! The Trust quorum relies on IPv6 multicast discovery, rack secret handling,
 //! and the SPDM protocol.
+//!
+//! Below is the trust quorum protocol for share retrieval over TCP.
+//!
+//! The following protocol is shown between two sleds only, but multicast
+//! discovery and share requests will continue to run until enough shares
+//! have been received to recreate the rack secret.
+//!
+//! Sled1                                      Sled2
+//! =====                                      =====
+//!  ||  ------- Multicast Discovery -------->  ||
+//!  ||                                         ||
+//!  ||  <-------- Respond with IP -----------  ||
+//!  ||                                         ||
+//!  ||  ---- Connect to TrustQuorum port --->  ||
+//!  ||                                         ||
+//!  ||  --------- SPDM Requests ------------>  ||
+//!  ||                                         ||
+//!  ||  <-------- SPDM Responses ------------  ||
+//!  ||                                         ||
+//!  ||  ----- SPDM Channel Established ------  ||
+//!  ||                                         ||
+//!  ||  --------- Request Share ------------>  ||
+//!  ||                                         ||
+//!  ||  <----------- Share ------------------  ||
+//!
 
 mod rack_secret;
 mod server;
