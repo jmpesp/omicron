4437234644e85ced751bdbc8174d1ec8f6913901
James MacMahon <james@oxide.computer>
2024-02-01 12:03:43 -0500

    [multisled] set sled index based on bootstrap ipv6
    note interface id is 1

--- sled-agent/src/rack_setup/plan/sled.rs
+++ sled-agent/src/rack_setup/plan/sled.rs
@@ -33,7 +33,7 @@ impl Plan {
         let rack_id = Uuid::new_v4();
 
         let bootstrap_addrs = bootstrap_addrs.into_iter().enumerate();
-        let allocations = bootstrap_addrs.map(|(idx, bootstrap_addr)| {
+        let allocations = bootstrap_addrs.map(|(_, bootstrap_addr)| {
             info!(log, "Creating plan for the sled at {:?}", bootstrap_addr);
             let bootstrap_addr = SocketAddrV6::new(
                 bootstrap_addr,
@@ -41,6 +41,42 @@ impl Plan {
                 0,
                 0,
             );
+
+            // XXX normally, each sled gets whatever index. problem is that
+            // we're not automatically discovering zpools for non-gimlets, so
+            // dataset requests will need to go to the right machine. use `dladm
+            // show-phys -m` to get MAC addresses and match here.
+            use sled_hardware::underlay::mac_to_bootstrap_ip;
+            let bootstrap_ipv6: Ipv6Addr = *bootstrap_addr.ip();
+            let interface_id = 1;
+
+            let idx =
+                // dinnerbone ixgbe0
+                if mac_to_bootstrap_ip("00:1b:21:c1:ff:e0".parse().unwrap(), interface_id) == bootstrap_ipv6 {
+                    0
+
+                // kibblesnbits ixgbe0
+                } else if mac_to_bootstrap_ip("00:1b:21:c1:fc:da".parse().unwrap(), interface_id) == bootstrap_ipv6 {
+                    1
+
+                // gravytrain ixgbe0
+                } else if mac_to_bootstrap_ip("00:1b:21:c1:fd:24".parse().unwrap(), interface_id) == bootstrap_ipv6 {
+                    2
+
+                // frostypaws ixgbe0 - ixgbe5
+                } else if mac_to_bootstrap_ip("80:61:5f:07:41:d8".parse().unwrap(), interface_id) == bootstrap_ipv6 ||
+                    mac_to_bootstrap_ip("80:61:5f:07:41:d9".parse().unwrap(), interface_id) == bootstrap_ipv6 ||
+                    mac_to_bootstrap_ip("80:61:5f:11:ab:30".parse().unwrap(), interface_id) == bootstrap_ipv6 ||
+                    mac_to_bootstrap_ip("80:61:5f:11:ab:31".parse().unwrap(), interface_id) == bootstrap_ipv6 ||
+                    mac_to_bootstrap_ip("8c:dc:d4:af:d2:a8".parse().unwrap(), interface_id) == bootstrap_ipv6 ||
+                    mac_to_bootstrap_ip("8c:dc:d4:af:d2:a9".parse().unwrap(), interface_id) == bootstrap_ipv6 {
+
+                    3
+
+                } else {
+                    panic!("unrecognized bootstrap addr {:?}", bootstrap_addr);
+                };
+
             let sled_subnet_index =
                 u8::try_from(idx + 1).expect("Too many peers!");
             let subnet = config.sled_subnet(sled_subnet_index);
