45b5e8ca33d5dbaca6e075f1865267e659579a86
James MacMahon <james@oxide.computer>
2024-02-01 12:34:49 -0500

    [multisled] mtu 9000 for switch zone maghemite links

--- sled-agent/src/services.rs
+++ sled-agent/src/services.rs
@@ -304,6 +304,9 @@ pub enum Error {
 
     #[error("Unexpected zone config: zone {zone_id} is running on ramdisk ?!")]
     ZoneIsRunningOnRamdisk { zone_id: OmicronZoneUuid },
+
+    #[error("Error setting linkprop")]
+    SetLinkprop(#[from] illumos_utils::dladm::SetLinkpropError),
 }
 
 impl Error {
@@ -1204,8 +1207,13 @@ impl ServiceManager {
                     // If on a non-gimlet, sled-agent can be configured to map
                     // links into the switch zone. Validate those links here.
                     for link in &self.inner.switch_zone_maghemite_links {
+                        let name = link;
                         match Dladm::verify_link(&link.to_string()) {
                             Ok(link) => {
+                                // Set MTU to 9000 before use, so as not to
+                                // fragment packets!
+                                Dladm::set_linkprop(&name.to_string(), "mtu", "9000")?;
+
                                 // Link local addresses should be created in the
                                 // zone so that maghemite can listen on them.
                                 links.push((link, true));
