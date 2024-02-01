fc2176398a841dd3e0fd5a6b4882349f7a55fab0
James MacMahon <james@oxide.computer>
2024-02-01 12:34:34 -0500

    [multisled] stub config does not need uplinkd, tfportd
    uplinkd smf configuration fails, so skip it

--- sled-agent/src/services.rs
+++ sled-agent/src/services.rs
@@ -4294,14 +4294,14 @@ impl ServiceManager {
                     SwitchService::Dendrite { asic },
                     SwitchService::Lldpd { baseboard: baseboard.clone() },
                     SwitchService::ManagementGatewayService,
-                    SwitchService::Uplink,
+                    //SwitchService::Uplink,
                     SwitchService::Wicketd { baseboard: baseboard.clone() },
                     SwitchService::Mgd,
                     SwitchService::MgDdm { mode: "transit".to_string() },
-                    SwitchService::Tfport {
-                        pkt_source: "tfpkt0".to_string(),
-                        asic,
-                    },
+                    //SwitchService::Tfport {
+                    //    pkt_source: "tfpkt0".to_string(),
+                    //    asic,
+                    //},
                     SwitchService::SpSim,
                 ]
             }
@@ -4395,6 +4395,8 @@ impl ServiceManager {
             }
         };
 
+        /*
+        // JWM don't need on my setup, no tofino
         info!(self.inner.log, "ensuring scrimlet uplinks");
         let usmfh = SmfHelper::new(&zone, &SwitchService::Uplink);
         let lsmfh = SmfHelper::new(
@@ -4465,6 +4467,7 @@ impl ServiceManager {
         }
         usmfh.refresh()?;
         lsmfh.refresh()?;
+        */
 
         Ok(())
     }
