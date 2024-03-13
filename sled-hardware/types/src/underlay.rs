db25916a70bd88787b776baba6e0b05a0d682195
James MacMahon <james@oxide.computer>
2024-03-13 16:09:10 -0400

    [multisled] mac_to_bootstrap_ip moved

--- sled-hardware/types/src/underlay.rs
+++ sled-hardware/types/src/underlay.rs
@@ -21,4 +21,46 @@ impl BootstrapInterface {
             BootstrapInterface::SwitchZone => 2,
         }
     }
+
+    // TODO(https://github.com/oxidecomputer/omicron/issues/945): This address
+    // could be randomly generated when it no longer needs to be durable.
+    pub async fn ip(
+        self,
+        link: &PhysicalLink,
+    ) -> Result<Ipv6Addr, dladm::GetMacError> {
+        let mac = Dladm::get_mac(link).await?;
+        Ok(mac_to_bootstrap_ip(mac, self.interface_id()))
+    }
+}
+
+pub fn mac_to_bootstrap_ip(mac: MacAddr, interface_id: u64) -> Ipv6Addr {
+    let mac_bytes = mac.into_array();
+    assert_eq!(6, mac_bytes.len());
+
+    Ipv6Addr::new(
+        BOOTSTRAP_PREFIX,
+        (u16::from(mac_bytes[0]) << 8) | u16::from(mac_bytes[1]),
+        (u16::from(mac_bytes[2]) << 8) | u16::from(mac_bytes[3]),
+        (u16::from(mac_bytes[4]) << 8) | u16::from(mac_bytes[5]),
+        ((interface_id >> 48) & 0xffff).try_into().unwrap(),
+        ((interface_id >> 32) & 0xffff).try_into().unwrap(),
+        ((interface_id >> 16) & 0xffff).try_into().unwrap(),
+        (interface_id & 0xfff).try_into().unwrap(),
+    )
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+    use macaddr::MacAddr6;
+
+    #[test]
+    fn test_mac_to_bootstrap_ip() {
+        let mac = MacAddr("a8:40:25:10:00:01".parse::<MacAddr6>().unwrap());
+
+        assert_eq!(
+            mac_to_bootstrap_ip(mac, 1),
+            "fdb0:a840:2510:1::1".parse::<Ipv6Addr>().unwrap(),
+        );
+    }
 }
