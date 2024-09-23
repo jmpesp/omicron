0316f79c7a9fa4dde6546f50b902a853ec9cedf2
James MacMahon <james@oxide.computer>
2024-09-23 20:01:06 +0000

    [multisled] new zone needs extra flag

--- sled-agent/src/services.rs
+++ sled-agent/src/services.rs
@@ -1600,6 +1600,7 @@ impl ServiceManager {
                     Some(&info.underlay_address),
                     &installed_zone,
                     &[*address.ip()],
+                    false,
                 )?;
 
                 let dns_service = Self::dns_install(info, None, None).await?;
