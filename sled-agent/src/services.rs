f4faecebbb817e1adafedee39ef1457c20c411bf
James MacMahon <james@oxide.computer>
2024-09-24 02:37:55 +0000

    [multisled] make sprockets work, fix for zone-network-setup

--- sled-agent/src/services.rs
+++ sled-agent/src/services.rs
@@ -1519,13 +1519,7 @@ impl ServiceManager {
             config_builder = config_builder.add_property(
                 "forwarding",
                 "astring",
-                "true",
-            );
-        } else {
-            config_builder = config_builder.add_property(
-                "forwarding",
-                "astring",
-                "false",
+                "-f",
             );
         }
 
