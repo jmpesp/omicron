7f6342d3f103696f40c99707dfbe4d36483ce8e6
James MacMahon <james@oxide.computer>
2024-10-01 16:03:50 +0000

    [multisled] another missing argument for zone

--- sled-agent/src/services.rs
+++ sled-agent/src/services.rs
@@ -2147,6 +2147,7 @@ impl ServiceManager {
                     Some(&info.underlay_address),
                     &installed_zone,
                     &[*address.ip()],
+                    false,
                 )?;
 
                 let rack_net =
