5c6aab2ec0c5fd8bc1c4a3f93abdbe0374e6512c
James MacMahon <james@oxide.computer>
2025-02-25 21:39:53 +0000

    [multicast] some simulated sp fixes

--- sp-sim/src/gimlet.rs
+++ sp-sim/src/gimlet.rs
@@ -887,6 +887,7 @@ impl Handler {
             serial_number,
             attached_mgs,
             incoming_serial_console,
+            power_state: PowerState::A0,
             startup_options: StartupOptions::empty(),
             update_state,
             reset_pending: None,
