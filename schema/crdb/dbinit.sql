aa0cca3f094a8b5d71c43141a7bbd3b716679440
James MacMahon <james@oxide.computer>
2025-08-25 21:22:35 +0000

    wip indexes

--- schema/crdb/dbinit.sql
+++ schema/crdb/dbinit.sql
@@ -996,12 +996,19 @@ CREATE TABLE IF NOT EXISTS omicron.public.silo_group (
 );
 
 CREATE UNIQUE INDEX IF NOT EXISTS
- lookup_silo_group_by_silo
+ lookup_silo_group_by_silo_and_external_id
 ON
  omicron.public.silo_group (silo_id, external_id)
 WHERE
  time_deleted IS NULL and
  (user_provision_type = 'api_only' OR user_provision_type = 'jit');
+
+CREATE UNIQUE INDEX IF NOT EXISTS
+ lookup_silo_group_by_silo_and_display_name
+ON
+ omicron.public.silo_group (silo_id, display_name)
+WHERE
+ time_deleted IS NULL AND user_provision_type = 'scim';
 
 /*
  * Silo group membership
