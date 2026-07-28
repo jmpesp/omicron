967e47e26b6ea84b4a068da201809a26c7d91136
James MacMahon <james@oxide.computer>
2026-07-28 15:54:57 +0000

    [multisled] allow adding whatever dns we want

--- dns-server/dnsadm/src/main.rs
+++ dns-server/dnsadm/src/main.rs
@@ -242,11 +242,7 @@ async fn main_impl() -> Result<()> {
 /// Verify that the given DNS zone name provided by the user falls under the
 /// ".oxide.test" name to ensure that it can never conflict with a real deployed
 /// zone name.
-fn verify_zone_name(zone_name: &str) -> Result<()> {
-    ensure!(
-        zone_name.trim_end_matches('.').ends_with(".oxide.test"),
-        "zone name must be under \".oxide.test\""
-    );
+fn verify_zone_name(_zone_name: &str) -> Result<()> {
     Ok(())
 }
 
